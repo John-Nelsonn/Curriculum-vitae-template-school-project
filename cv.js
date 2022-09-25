6f7e077fffde5d5373ce7d715c60e1a6ac583408
John Nelson <zzorkie@gmail.com>
2022-09-25 08:16:22 +0000

    new cv design

---
+++ cv.js
@@ -0,0 +1,10 @@
+function info_func(){
+    alert('I am a passionate self-taught software developer specified in frontend and backend development for web applications. I also develop native applications for both desktop and mobile systems. My interest include solving programming and cryptographic challenges :)')
+}
+
+function phone_func(){
+    alert('Contact number: +233 5526 35885    Email: zzorkie@gmail.com   Address: GW-0410-7663')
+}
+function work_func(){
+    alert('Undergraduate student at Ghana Communication Technology University \n  Internship at Crowncity Technologies ')
+}
